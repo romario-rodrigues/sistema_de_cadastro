20dda16fe859fefac536f8912811223f349fe7de
Romario Rodrigues <romariobdc2011@gmail.com>
2023-12-10 16:39:18 -0300

    Terminado inclusão e listagem de nomes de usuários

--- projeto_cad_usuarios/projeto_cad_usuarios/urls.py
+++ projeto_cad_usuarios/projeto_cad_usuarios/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from app_cad_usuarios import views
+from app_cad_usuarios import views # Erro corrigido nessa linha no app_cad_usuarios. Faltava o s em usuarios
 
 urlpatterns = [
     # rota, view responsável, nome de referência
